c2ee63e301b36e5d36a9606f8faf27c5b615e346
Shounak <shounakpurnapatre1@gmail.com>
2024-06-23 01:03:58 +0530

    Second commit

--- Algorithm.java
+++ Algorithm.java
@@ -4,7 +4,7 @@ public class Algorithm {
     public static void main(String[] args) {
    
             System.out.println("I am inside.");
-        
+        System.out.println("Process should get automate");
 
     }
 }
